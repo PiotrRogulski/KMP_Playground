c0a4fb4ce7c25276d7fbd0babc1f39a39ac5e12b
PiotrRogulski <piotr.rogulski@leancode.pl>
2024-06-20 09:39:48 +0200

    Add graphics layer demo

--- composeApp/src/commonMain/kotlin/features/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/features/home/HomeScreen.kt
@@ -1,7 +1,13 @@
 package features.home
 
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.unit.*
 import common.widgets.*
 
 @Composable
@@ -9,6 +15,33 @@ fun HomeScreen() {
     AppScaffold(title = "Home") {
         item {
             Text("Home Screen")
+        }
+        item {
+            val angleX by rememberInfiniteTransition().animateFloat(
+                initialValue = 0f,
+                targetValue = 360f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(4000, easing = LinearEasing),
+                ),
+            )
+            val angleY by rememberInfiniteTransition().animateFloat(
+                initialValue = 0f,
+                targetValue = 360f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(5300, easing = LinearEasing),
+                ),
+            )
+            Box(Modifier.padding(64.dp).size(250.dp).graphicsLayer {
+                rotationX = angleX
+                rotationY = angleY
+            }) {
+                Box(Modifier.background(Color.Green).fillMaxSize())
+                Text(
+                    "Box with graphicsLayer",
+                    Modifier.align(Alignment.Center),
+                    Color.Black,
+                )
+            }
         }
     }
 }
