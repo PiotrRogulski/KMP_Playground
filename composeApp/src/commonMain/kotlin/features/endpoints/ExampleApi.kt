6bcb64428a3475a02c2df82e4096f784387ddee3
PiotrRogulski <piotr.rogulski@leancode.pl>
2024-07-01 13:24:05 +0200

    Refactor API

--- composeApp/src/commonMain/kotlin/features/endpoints/ExampleApi.kt
+++ composeApp/src/commonMain/kotlin/features/endpoints/ExampleApi.kt
@@ -10,24 +10,23 @@ import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.*
 
-class ExampleApi {
-    private val client = HttpClient(CIO) {
-        defaultRequest {
-            url("https://reqres.in/api/")
-        }
-
-        install(ContentNegotiation) {
-            json(Json { ignoreUnknownKeys = true })
-        }
-    }
-
-    suspend fun getUsers(
-        page: Int = 1,
-        perPage: Int = 4,
-    ): PaginatedResponse<User> = client.get("users") {
-        parameter("page", page)
-        parameter("per_page", perPage)
-    }.body()
-
-    suspend fun getUser(id: Int): SingleResponse<User> = client.get("users/$id").body()
+class ExampleApi(
+    val client: HttpClient = HttpClient(CIO) {
+        defaultRequest { url("https://reqres.in/api/") }
+        install(ContentNegotiation) { json(Json { ignoreUnknownKeys = true }) }
+    },
+) {
+    suspend fun getUsers(page: Int = 1, perPage: Int = 4) = paginated<User>("users", page, perPage)
+    suspend fun getUser(id: Int) = single<User>("users/$id")
 }
+
+private suspend inline fun <reified T> ExampleApi.single(query: String): SingleResponse<T> = client.get(query).body()
+
+private suspend inline fun <reified T> ExampleApi.paginated(
+    query: String,
+    page: Int = 1,
+    perPage: Int = 4,
+): PaginatedResponse<T> = client.get(query) {
+    parameter("page", page)
+    parameter("per_page", perPage)
+}.body()
